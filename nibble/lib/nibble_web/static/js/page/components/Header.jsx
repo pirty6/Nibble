55d536b47a95e06d2b435872508645beda9ac4fd
icecreamsoldier <mariana.9000@hotmail.com>
2017-08-30 15:34:20 -0500

    Header structure finished

--- nibble/lib/nibble_web/static/js/page/components/Header.jsx
+++ nibble/lib/nibble_web/static/js/page/components/Header.jsx
@@ -6,8 +6,10 @@ class Header extends Component{
     return (
       <div className = 'header'>
         <div className = 'container'>
-          <h1>{ this.props.title }</h1>
-          <h2>{ this.props.subtitle }</h2>
+          <div className = 'container-text'>
+            <h1>{ this.props.title }</h1>
+            <h2>{ this.props.subtitle }</h2>
+          </div>
         </div>
         <div className = 'image-container'
           style = {{ backgroundImage: 'url('+ this.props.image.desktop +')' }}>
