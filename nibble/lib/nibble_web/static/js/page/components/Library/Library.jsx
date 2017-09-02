936db03a88eda89e46281a41067a3a6e9e15e36e
icecreamsoldier <mariana.9000@hotmail.com>
2017-09-02 09:55:25 -0500

    Modal information show

--- nibble/lib/nibble_web/static/js/page/components/Library/Library.jsx
+++ nibble/lib/nibble_web/static/js/page/components/Library/Library.jsx
@@ -3,14 +3,69 @@ import React, { Component } from 'react';
 import Header from './../Header.jsx';
 
 class Library extends Component{
+  constructor(props) {
+    super(props);
+    this.showInformation = this.showInformation.bind(this);
+    this.renderInformation = this.renderInformation.bind(this);
+  }
+
+  state = {
+    element: null,
+  };
+
+  //function for returning the information
+  //of a specific element in the modal
+  showInformation(element) {
+    this.setState({ element });
+  }
+
+  renderInformation(element) {
+    if (element) {
+      return (
+        <div className = 'modal'>
+          <div className = 'close'>
+            <div role = 'button' tabIndex = {0}
+              onClick = { this.props.toggleModal }>
+                X
+              </div>
+          </div>
+          <div className = 'container'>
+            <div className = 'left-column'>
+
+            </div>
+            <div className = 'right-column'>
+              <h2>{ element.title }</h2>
+              <p>Autor: { element.author }</p>
+              <p>Editorial: { element.editorial }</p>
+              <p>codigo: { element.id }</p>
+              <p>Descripcion: { element.description }</p>
+              { element.genre.map((tag, i) => (
+                <span key = { i }>{ tag }</span>
+              ))}
+            </div>
+          </div>
+        </div>
+      );
+    }
+
+    return null;
+  }
+
   render() {
     const {
       header,
       books,
+      toggleModal,
+      toggle,
     } = this.props;
 
     let book = books.map((element, index) => (
       <div className = 'book' key = { index }
+        onClick = {() => {
+        this.showInformation(element);
+        toggleModal();
+      }}
+
         style = { element.image
           ? { backgroundImage: 'url(' + element.image + ')' }
         : { backgroundColor: '#7AC9DD' } }>
@@ -29,6 +84,9 @@ class Library extends Component{
         <div className = 'books-container'>
           <div className = 'container'>
             { book }
+            <div className = {`modal ${toggle ? 'show' : 'hidden'}`}>
+              { this.renderInformation(this.state.element) }
+            </div>
           </div>
         </div>
       </div>
--- nibble/lib/nibble_web/static/js/page/containers/Library.jsx
+++ nibble/lib/nibble_web/static/js/page/containers/Library.jsx
@@ -2,17 +2,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Library from '../components/Library/Library.jsx';
+import { toggleModal } from '../actions/app';
 
 function mapStateToProps(state) {
   return {
     header: state.library.header,
     books: state.library.books,
+    toggle: state.modal.toggle,
   };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
-
+    toggleModal: () => (dispatch(toggleModal())),
   };
 }
 
