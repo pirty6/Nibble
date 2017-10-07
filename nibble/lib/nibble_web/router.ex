c203d2123df497809ae8238e8333674ef4d20004
David Ramirez <davidsrnfire@gmail.com>
2017-10-07 10:27:14 -0500

    Updated Router

--- nibble/lib/nibble_web/router.ex
+++ nibble/lib/nibble_web/router.ex
@@ -43,6 +43,12 @@ defmodule NibbleWeb.Router do
     resources "/", PlaceController, except: [:new, :edit]
   end
 
+  scope "/api/users", NibbleWeb do
+    pipe_through :browser
+
+    resources "/", UserController
+  end
+
   # Other scopes may use custom stacks.
   # scope "/api", NibbleWeb do
   #   pipe_through :api
