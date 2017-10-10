4b9eaf6b3cd99a875d746e38101bc7c0b2e4ebd1
David Ramirez <davidsrnfire@gmail.com>
2017-10-10 15:49:54 -0500

    Finished all tests

--- nibble/test/nibble_web/controllers/book_controller_test.exs
+++ nibble/test/nibble_web/controllers/book_controller_test.exs
@@ -18,59 +18,59 @@ defmodule NibbleWeb.BookControllerTest do
   end
 
   describe "index" do
-    test "lists all books", %{conn: conn} do
-      conn = get conn, book_path(conn, :index)
-      assert html_response(conn, 200)["data"] == []
+    test "renders form", %{conn: conn} do
+      conn = get conn, book_path(conn, :new)
+      assert html_response(conn, 200) =~ "New Book"
+    end
+  end
+
+  describe "new book" do
+    test "renders form", %{conn: conn} do
+      conn = get conn, book_path(conn, :new)
+      assert html_response(conn, 200) =~ "New Book"
     end
   end
 
   describe "create book" do
-    test "renders book when data is valid", %{conn: conn} do
+    test "redirects to show when data is valid", %{conn: conn} do
       conn = post conn, book_path(conn, :create), book: @create_attrs
-      assert %{"id" => id} = json_response(conn, 201)["data"]
+
+      assert %{id: id} = redirected_params(conn)
+      assert redirected_to(conn) == book_path(conn, :show, id)
 
       conn = get conn, book_path(conn, :show, id)
-      assert json_response(conn, 200)["data"] == %{
-        "id" => id,
-        "author" => "some author",
-        "code" => 42,
-        "description" => "some description",
-        "editorial" => "some editorial",
-        "genre" => "some genre",
-        "title" => "some title",
-        "urlimg" => "some urlimg",
-        "urlpdf" => "some urlpdf"}
+      assert html_response(conn, 200) =~ "Show Book"
     end
 
     test "renders errors when data is invalid", %{conn: conn} do
       conn = post conn, book_path(conn, :create), book: @invalid_attrs
-      assert json_response(conn, 422)["errors"] != %{}
+      assert html_response(conn, 200) =~ "New Book"
+    end
+  end
+
+  describe "edit book" do
+    setup [:create_book]
+
+    test "renders form for editing chosen book", %{conn: conn, book: book} do
+      conn = get conn, book_path(conn, :edit, book)
+      assert html_response(conn, 200) =~ "Edit Book"
     end
   end
 
   describe "update book" do
     setup [:create_book]
 
-    test "renders book when data is valid", %{conn: conn, book: %Book{id: id} = book} do
+    test "redirects when data is valid", %{conn: conn, book: book} do
       conn = put conn, book_path(conn, :update, book), book: @update_attrs
-      assert %{"id" => ^id} = json_response(conn, 200)["data"]
+      assert redirected_to(conn) == book_path(conn, :show, book)
 
-      conn = get conn, book_path(conn, :show, id)
-      assert json_response(conn, 200)["data"] == %{
-        "id" => id,
-        "author" => "some updated author",
-        "code" => 43,
-        "description" => "some updated description",
-        "editorial" => "some updated editorial",
-        "genre" => "some updated genre",
-        "title" => "some updated title",
-        "urlimg" => "some updated urlimg",
-        "urlpdf" => "some updated urlpdf"}
+      conn = get conn, book_path(conn, :show, book)
+      assert html_response(conn, 200) =~ "some updated author"
     end
 
     test "renders errors when data is invalid", %{conn: conn, book: book} do
       conn = put conn, book_path(conn, :update, book), book: @invalid_attrs
-      assert json_response(conn, 422)["errors"] != %{}
+      assert html_response(conn, 200) =~ "Edit Book"
     end
   end
 
@@ -79,7 +79,7 @@ defmodule NibbleWeb.BookControllerTest do
 
     test "deletes chosen book", %{conn: conn, book: book} do
       conn = delete conn, book_path(conn, :delete, book)
-      assert response(conn, 204)
+      assert redirected_to(conn) == book_path(conn, :index)
       assert_error_sent 404, fn ->
         get conn, book_path(conn, :show, book)
       end
