1bdece1b631fb9fdc1db6270eef2ec91c080ee33
David Ramirez <davidsrnfire@gmail.com>
2017-11-21 15:30:58 -0600

    Added Dashboard

--- nibble/lib/nibble_web/router.ex
+++ nibble/lib/nibble_web/router.ex
@@ -84,7 +84,7 @@ defmodule NibbleWeb.Router do
   scope "/cms", NibbleWeb do
     pipe_through [:browser,:browser_auth,:ensure_authed_access]
     # get "/*path", PageController, :apps
-    # get "/", UserController, :index
+    get "/dashboard", UserController, :index
     resources "/libreria", BookController
     resources "/usuarios", UserController
     resources "/sectores", PlaceController
